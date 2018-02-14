85d6ec31ef5cf4bcd49ecd1c1926951b516f2804
nafeesadil <adil.naffy@gmail.com>
2018-02-14 09:32:00 +0600

    classwork 8

--- smartForm.js
+++ smartForm.js
@@ -1,42 +1,46 @@
 // Your jQuery goes here
 
 var userData = {
-	name: '',
-	email: '',
-	html: {likes:[],dislikes: []},
-	css: {likes:[],dislikes: []},
-	js: {likes:[],dislikes: []},
-	strength: {css: '',js: '', html:''},
-	currentQuestion: '#welcome'
-
+  name:'',
+  email:'',
+  html:{likes:[], dislikes:[]},
+  css:{likes:[], dislikes:[]},
+  js:{likes:[], dislikes:[]},
+  strength: {css:'', js:'', html:''},
+  currentQuestion: '#welcome'
 };
-$('#start').click(function()
-{
+console.log(localStorage.getItem(userData));
+if(localStorage.getItem('userData')){
+  userData=JSON.parse(localStorage.getItem('userData'));
+   $("#welcome").hide();
+    $(userData.currentQuestion).show();
+   $('#name').val(userData.name);
+   $('email').val(userData.email);
+}else{
+  localStorage.setItem('userData',JSON.stringify(userData));
+}
 
-	//console.log("start");
-	$('#welcome').hide();
-	$('#q1').show();
-	
+
+$("#start").click(function(){
+  $("#welcome").hide();
+  $("#q1").show();
+  userData.currentQuestion ="#q1";
+  localStorage.setItem('userData',JSON.stringify(userData));
 });
-$('#name').change(function(event)
-{
 
-	console.log($('#name').val()); //couts whatever is in the form
+$("#name").change(function(event){
+  //console.log(event);
+  console.log($("#name").val());
 });
-
-$('#q1next').click(function()
-{
-
-	if($('#name').val() && $('#email').val())
-	{
-		userData.name = $('#name').val();
-		userData.email = $('#email').val();
-		$('#q2').show();
-		$('#q1').hide();
-
-	}
-	else
-	{
-		alert('please enter your e mail');
-	}
+$("#q1next").click(function(){
+  if($('#name').val() && $('#email').val()){
+    userData.name = $('#name').val();
+    userData.email = $('#email').val();
+    console.log(userData);
+    localStorage.setItem('userData',JSON.stringify(userData));
+    $("#q2").show();
+    $("#q1").hide();
+  }else {
+    alert("Please enter name and email");
+  }
 });
